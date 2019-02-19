d5bb14506cccb39cfc015d112865acda925cc794
jbj616 <jbj616@gmail.com>
2019-02-20 00:37:28 +0900

    2/19

--- src/Baekjoon_2293.java
+++ src/Baekjoon_2293.java
@@ -7,17 +7,18 @@ public class Baekjoon_2293 {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] coin = new int[k+1];
+        int[] coin = new int[n+1];
         int[] dp = new int[k+1];
 
-        for(int i=1; i<=n; i++){
-            coin[sc.nextInt()]++;
+        for (int i=1; i<=n;i++){
+            coin[i]=sc.nextInt();
         }
+        dp[0] = 1;
 
         for (int i=1; i<=n; i++){
-            for (int j=1; j<=i; i++){
-
-            }
+            for (int j=coin[i]; j<=k; j++)
+                dp[j] +=dp[j-coin[i]];
         }
+        System.out.println(dp[k]);
     }
 }
