20bd004ab2da990077bff7398b09aea72af0baaf
jbj616 <jbj616@gmail.com>
2019-02-20 18:22:31 +0900

    2/20

--- src/Baekjoon_2443.java
+++ src/Baekjoon_2443.java
@@ -7,7 +7,12 @@ public class Baekjoon_2443 {
         int n = sc.nextInt();
 
         for (int i=0 ;i< n; i++){
-            
+            for (int j=0; j<i; j++)
+                System.out.print(" ");
+            for (int j=0; j<2*(n-i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
