d924e2aa5a08fc65a98b588afa0c615c9c627885
jbj616 <jbj616@gmail.com>
2019-02-24 22:59:06 +0900

    2/24

--- src/Baekjoon_1181.java
+++ src/Baekjoon_1181.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Baekjoon_1181 {
     public static void main(String[] args) {
@@ -7,14 +6,36 @@ public class Baekjoon_1181 {
 
         int n = sc.nextInt();
         String[] str = new String[n];
+        int maxLen = 0;
+        ArrayList<String> array = new ArrayList<>();
+        Ascending ascending = new Ascending();
 
         for(int i=0; i< n; i++){
             str[i] = sc.next();
+            if(maxLen < str[i].length())
+                maxLen = str[i].length();
         }
 
-        Arrays.sort(str);
+        for(int i=1; i<=maxLen;i++){
+            for (int j=0; j<str.length; j++){
+                if(i == str[j].length()){
+                   if(!array.contains(str[j]))
+                       array.add(str[j]);
+                }
+            }
+            Collections.sort(array, ascending);
 
-        for (int i=0; i<n; i++)
-            System.out.println(str[i]);
+            int size = array.size();
+            for(int j=0; j<size; j++)
+                System.out.println(array.remove(0));
+        }
     }
+}
+class Ascending implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.compareTo(o2);
+    }
+
 }
