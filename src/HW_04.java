f227f79ae4351091b39cf782683da19bccace768
jbj616 <jbj616@gmail.com>
2019-05-31 17:50:34 +0900

    chore

--- src/HW_04.java
+++ src/HW_04.java
@@ -7,42 +7,40 @@ import java.util.Scanner;
 public class HW_04 {
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
-        try{
-            Scanner sc = new Scanner(new File("virus02.in"));
+        try {
+            Scanner sc = new Scanner(new File("virus08.in"));
             int n = sc.nextInt();
-            Queue<Integer> leftQ = new LinkedList<>();
-            Queue<Integer> rightQ = new LinkedList<>();
+            Queue<Integer> leftNode = new LinkedList<>();
+            Queue<Integer> rightNode = new LinkedList<>();
+
             for (int i = 0; i < n; i++) {
-                leftQ.add(sc.nextInt());
-                rightQ.add(sc.nextInt());
+                leftNode.add(sc.nextInt());
+                rightNode.add(sc.nextInt());
             }
-            System.out.println(virus(n,leftQ, rightQ));
-        }catch (FileNotFoundException e){
+            System.out.println(virus(leftNode, rightNode));
+        } catch (FileNotFoundException e) {
             System.out.println(e);
         }
-
-        System.out.println("걸린 시간: " + (System.currentTimeMillis()-time));
-
+        System.out.println("걸린 시간: " + (System.currentTimeMillis() - time));
     }
 
-    public static int virus(int n, Queue<Integer> leftQ, Queue<Integer> rightQ) {
+    public static int virus(Queue<Integer> leftNode, Queue<Integer> rightNode) {
         int height = 1;
         int sub = 1;
-        for (int k=0; k<n;k++){
-            for (int i = 0; i < sub; i++) {
-                int a = leftQ.poll();
-                int b = rightQ.poll();
+        while (!leftNode.isEmpty()) {
+            for (int i = 0; i < sub; i++) { // 높이단위록 검사
+                int a = leftNode.poll();
+                int b = rightNode.poll();
 
                 if ((a == 0 && b != 0) || (b == 0 && a != 0)) {
-                    return height ;
+                    return height;
                 } else if (a == 0 && b == 0) {
-                    return height-1;
+                    return height;
                 }
             }
-            height++;
             sub *= 2;
+            height++;
         }
-        System.out.println("---");
-        return height ;
+        return height;
     }
 }
