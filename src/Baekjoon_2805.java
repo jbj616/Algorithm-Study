c518443985b17007d63209409d06e6c1de20cbd7
jbj616 <jbj616@gmail.com>
2019-10-12 11:00:04 +0900

    10/11 스터디

--- src/Baekjoon_2805.java
+++ src/Baekjoon_2805.java
@@ -9,7 +9,7 @@ public class Baekjoon_2805 {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        int[] tree = new int[n + 1];
+        int[] tree = new int[n];
 
         tree[0] = 0;
         for (int i = 1; i < tree.length; i++) {
@@ -18,40 +18,26 @@ public class Baekjoon_2805 {
 
         Arrays.sort(tree);
 
-        int answer = -1;
-        int index = -1;
-        int tmp2 = -1;
-        for (int i = tree.length - 1; i >= 0; i--) {
-            tmp2 = 0;
-            for (int j = i + 1; j < tree.length; j++) {
-                tmp2 += (tree[j] - tree[i]);
+        int low = 0;
+        int high = n - 1;
+        int mid;
+        int total  = 0;
+
+        while (low < high) {
+            mid = (low + high) / 2;
+
+            for (int i = mid; i < n; i++) {
+                total = (tree[i] - tree[mid]);
             }
 
-            if (tmp2 >= m) {
-                answer = tree[i];
-                index = i;
+            if(total == m){
                 break;
-            }
+            }else if(total < m){
+                high = mid -1;
+            }else{
 
-        }
-
-        if (tmp2 == m) {
-            System.out.println(answer);
-        } else {
-            int tmp = 0;
-            for (int i = tree[index+1] - tree[index]; i >0 ; i--) {
-                tmp += tree.length - index;
-                if (tmp2 + tmp == m) {
-                    System.out.println(answer + i);
-                    break;
-                } else if (tmp2 + tmp > m) {
-                    System.out.println(answer +i - 1);
-                    break;
-                }
             }
         }
-
-
     }
 
 }
